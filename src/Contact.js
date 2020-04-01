a921b176dd7045ee81b8eb9e4eafd82e10f9cd0c
Yang YU <yuyangyyy@gmail.com>
2020-04-01 11:44:03 +0200

    Add a contact component.

---
+++ src/Contact.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <img
+        className="avatar"
+        src="https://randomuser.me/api/portraits/women/88.jpg"
+      />
+
+      <div>
+        <h1 className="name">Anitala</h1>
+
+        <div className="status">
+          <div className="status-online"></div>
+          <div className="status-text">Online</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
