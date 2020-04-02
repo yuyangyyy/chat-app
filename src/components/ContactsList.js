b565e19e1179e990a53454151d765e892d56abbe
Yang YU <yuyangyyy@gmail.com>
2020-04-02 17:23:05 +0200

    Add components.

---
+++ src/components/ContactsList.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Contact from './Contact'
+
+const users = [
+  {
+    name: "Robert Reyes",
+    avatar: "https://randomuser.me/api/portraits/men/28.jpg",
+    online: false
+  },
+  {
+    name: "Nellie Caldwell",
+    avatar: "https://randomuser.me/api/portraits/women/17.jpg",
+    online: true
+  },
+  {
+    name: "Vernon Mason",
+    avatar: "https://randomuser.me/api/portraits/men/84.jpg",
+    online: true
+  },
+  {
+    name: "Erica Hunt",
+    avatar: "https://randomuser.me/api/portraits/women/87.jpg",
+    online: false
+  },
+  {
+    name: "Juanita Phillips",
+    avatar: "https://randomuser.me/api/portraits/women/55.jpg",
+    online: true
+  }
+];
+
+const ContactsList = () => {
+  return (
+    <div className="userName">
+      {users.map(user => {
+        return (
+          <Contact name={user.name} src={user.avatar} online={user.online} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default ContactsList;
